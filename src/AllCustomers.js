bfac8a6f648b6e520d00fe09c268e126fc542722
Danny Warren <danny.warren@emmersionlearning.com>
2020-04-01 19:23:32 -0600

    Use api in https://github.com/helio-training/dr-20-01-cars-demo

--- src/AllCustomers.js
+++ src/AllCustomers.js
@@ -6,16 +6,38 @@ export default class AllCustomers extends Component {
         title: 'Boring Co. Customers'
     }
 
+    componentDidUpdate(prevProps) {
+        if (prevProps.customer !== this.props.customer){
+            console.log('Customer changed: from:', prevProps.customer, 'to:', this.props.customer)
+        }
+
+        if (prevProps.customers !== this.props.customers) {
+            console.log('Customers changed from->to', prevProps.customers, this.props.customers)
+        }
+    }
+
+    createCustomers = () => {
+        if (!this.props.customers.length) {
+            return (
+                <p>Loading Customers...</p>
+            )
+        }
+
+        const customersAsComponents = this.props.customers
+            .map(customer => (
+                <li key={customer._id}><Customer name={customer.name} /></li>
+            ))
+
+        return customersAsComponents
+    }
+
     render() {
         console.log('Step 5: Pass this.props.customer.name to Customer', this.props)
         return (
             <div>
                 <h1>{this.props.title}</h1>
                 <ul>
-                    <li><Customer name={this.props.customer.name} /></li>
-                    <li><Customer name="Danny" /></li>
-                    <li><Customer name="Billy Bob" /></li>
-                    <li><Customer name="Jonathan" /></li>
+                    {this.createCustomers()}
                 </ul>
             </div>
         )
--- src/App.js
+++ src/App.js
@@ -5,12 +5,56 @@ import './App.css'
 
 export default class App extends React.Component {
   state = {
-    customerData: { name: 'No customer saved' }
+    allCustomers: []
   }
 
-  saveCustomer = (customerToSave) => {
+  saveCustomer = async (customerToSave) => {
     console.log('Step 3: save NewCustomer data in App via setState', 'state:', this.state, 'customerToSave:', customerToSave)
-    this.setState({ customerData: customerToSave })
+    const postNewCustomerResponse = await fetch('http://localhost:4000/customers', {
+      method: 'POST',
+      mode: 'cors',
+      headers: {
+        'content-type': 'application/json; charset=utf-8'
+      },
+      body: JSON.stringify(customerToSave)
+    })
+
+    const savedCustomer = await postNewCustomerResponse.json()
+
+    //OPTION 1:
+    //this.setState({ allCustomers: [...this.state.allCustomers, savedCustomer] })
+
+    //OPTION 2: (recommended)
+    await this.refreshAllCustomers()
+  }
+
+  refreshAllCustomers = async () => {
+    const allCustomersResponse = await fetch('http://localhost:4000/customers', {
+      method: 'GET',
+      mode: 'cors',
+      headers: {
+        'accept': 'application/json; charset=utf-8',
+      }
+    })
+
+    const allCustomers = await allCustomersResponse.json()
+
+    console.log('allCustomers', allCustomers)
+
+    this.setState({ allCustomers })
+  }
+
+  async componentDidMount() {
+    console.log('App.componentDidMount -> Run API calls here...')
+    //consider having default values while waiting for data...
+
+    await this.refreshAllCustomers()
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.allCustomers !== this.state.custallCustomersomerData) {
+      console.log('allCustomers changed: from:', prevState.allCustomers, 'to:', this.state.custallCustomersomerData)
+    }
   }
 
   render() {
@@ -18,9 +62,13 @@ export default class App extends React.Component {
     return (
       <div className="App">
         <AllCustomers title="Danny's Car Shop"
-                      customer={this.state.customerData} />
+          customers={this.state.allCustomers} />
         <NewCustomer save={this.saveCustomer} />
       </div>
     )
+  }
+
+  componentWillUnmount() {
+    console.log('App.componentWillUnmount')
   }
 }
